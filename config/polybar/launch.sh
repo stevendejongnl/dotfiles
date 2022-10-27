207abcf23ecb4e2501d898f4eee82c57d833e4e1
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2022-10-27 09:23:41 +0200

    Cleanup polybar

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -7,4 +7,3 @@ killall -q polybar
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
 polybar main 2>&1 | tee -a /tmp/polybar-main.log & disown
-# polybar 2>&1 | tee -a /tmp/polybar-main.log & disown
