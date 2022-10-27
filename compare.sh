15285550a035cd5d37cf2cce444f6fd7888daa07
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2022-10-27 17:18:50 +0200

    Compare script two ways

--- compare.sh
+++ compare.sh
@@ -8,17 +8,35 @@ if [[ -z "$DIFFTOOL" ]]; then
 	exit
 fi
 
-read -r -p "Compare .config files [Y/n] " compare_config_files
+LOCAL_TO_REMOTE="false"
+read -r -p "Compare local to remote? [Y/n]" compare_config_files
 if [[ ! "$compare_config_files" =~ ^([nN][oO]|[nN])$ ]]; then
-	$DIFFTOOL "$HOME/.config" "$REPODIR/config"
+	LOCAL_TO_REMOTE="true"
 fi
 
-read -r -p "Compare .zshrc file [Y/n] " compare_zshrc_file
+read -r -p "Compare .config files? [Y/n] " compare_config_files
+if [[ ! "$compare_config_files" =~ ^([nN][oO]|[nN])$ ]]; then
+	if [[ "${LOCAL_TO_REMOTE}" == "true" ]]; then
+		$DIFFTOOL "$HOME/.config" "$REPODIR/config"
+	else
+		$DIFFTOOL "$REPODIR/config" "$HOME/.config"
+	fi
+fi
+
+read -r -p "Compare .zshrc file? [Y/n] " compare_zshrc_file
 if [[ ! "$compare_zshrc_file" =~ ^([nN][oO]|[nN])$ ]]; then
-	$DIFFTOOL "$HOME/.zshrc" "$REPODIR/zshrc"
+	if [[ "${LOCAL_TO_REMOTE}" == "true" ]]; then
+		$DIFFTOOL "$HOME/.zshrc" "$REPODIR/zshrc"
+	else
+		$DIFFTOOL "$REPODIR/zshrc" "$HOME/.zshrc"
+	fi
 fi
 
-read -r -p "Compare .aliases file [Y/n] " compare_aliases_file
+read -r -p "Compare .aliases file? [Y/n] " compare_aliases_file
 if [[ ! "$compare_aliases_file" =~ ^([nN][oO]|[nN])$ ]]; then
-	$DIFFTOOL "$HOME/.aliases" "$REPODIR/aliases"
+	if [[ "${LOCAL_TO_REMOTE}" == "true" ]]; then
+		$DIFFTOOL "$HOME/.aliases" "$REPODIR/aliases"
+	else
+		$DIFFTOOL "$REPODIR/aliases" "$HOME/.aliases"
+	fi
 fi
