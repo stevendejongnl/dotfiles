4079573c27acc6462cc846d73d108b9c4ce659a2
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2022-11-08 09:03:26 +0100

    Add config for multimonitor (office)

--- config/polybar/launch.sh
+++ config/polybar/launch.sh
@@ -6,4 +6,14 @@ killall -q polybar
 # Wait until the processes have been shut down
 while pgrep -x polybar >/dev/null; do sleep 1; done
 
-polybar main 2>&1 | tee -a /tmp/polybar-main.log & disown
+WORKSPACE=$(autorandr --current)
+
+if [[ $WORKSPACE == "office" ]]; then
+    MONITOR=DisplayPort-0 polybar main 2>&1 | tee -a /tmp/polybar-main.log & disown
+    MONITOR=eDP polybar secondary 2>&1 | tee -a /tmp/polybar-secondary.log & disown
+elif [[ $WORKSPACE == "office2" ]]; then
+    MONITOR=HDMI-A-0 polybar main 2>&1 | tee -a /tmp/polybar-main.log & disown
+    MONITOR=eDP polybar secondary 2>&1 | tee -a /tmp/polybar-secondary.log & disown
+else
+    polybar main 2>&1 | tee -a /tmp/polybar-main.log & disown
+fi
