fcbc56993c97370aab9d6caca60c777e679014c5
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2022-12-19 18:43:45 +0100

    Add bspwm

---
+++ roles/bspwm/files/rules.sh
@@ -0,0 +1,6 @@
+#! /bin/sh
+
+# The bspc rule -a command only uses XDG class names, which can be easily found out by using xprop:
+# xprop | grep WM_CLASS
+
+bspc config external_rules_command $BSPWM_CONFIG/external_rules.sh
