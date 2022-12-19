7027868cbabaeb5fa86a06902d88d2883e14909d
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2022-12-19 16:51:59 +0100

    File permission

--- roles/yay/files/install.sh
+++ roles/yay/files/install.sh
@@ -1,6 +1,11 @@
 #! /bin/bash
 
-pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
+if [ ! -d "yay-bin" ] ; then
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+else
+    cd yay-bin
+    git pull https://aur.archlinux.org/yay-bin.git
+fi
+
 makepkg -si
