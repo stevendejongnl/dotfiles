5a8174172ba6b421658f4453d9695dc5415233ae
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2022-12-19 17:53:25 +0100

    Use home dir

--- install-modules.sh
+++ install-modules.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 
 # Create the user custom module directory
-mkdir ~/.ansible/plugins/modules
+mkdir $HOME/.ansible/plugins/modules
 
 # Install the aur module into the user custom module directory
-curl -o ~/.ansible/plugins/modules/aur.py https://raw.githubusercontent.com/kewlfft/ansible-aur/master/plugins/modules/aur.py
+curl -o $HOME/.ansible/plugins/modules/aur.py https://raw.githubusercontent.com/kewlfft/ansible-aur/master/plugins/modules/aur.py
