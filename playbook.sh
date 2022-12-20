6aa6589bb725f6374705b6b75e6a25bee29048cb
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2022-12-20 18:12:55 +0100

    Add verbose

--- playbook.sh
+++ playbook.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
+if [[ "$s1" == "-v" ]] || [[ "$s1" == "--verbose" ]]
+    ansible-playbook --ask-become-pass --verbose roles.yml
+    exit 0
+fi
+
 ansible-playbook --ask-become-pass roles.yml
