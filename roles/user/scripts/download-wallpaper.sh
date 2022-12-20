cb7ca09da8ed8ee559185ac89a897b2858552010
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2022-12-20 22:00:08 +0100

    Follow redirect

--- roles/user/scripts/download-wallpaper.sh
+++ roles/user/scripts/download-wallpaper.sh
@@ -3,5 +3,5 @@
 if [[ "$1" == "--directory" ]]; then
     mkdir -p $2
 
-    curl -o "$2/lucas-segers-6mNKUrwMwFk-unsplash.jpg" "https://unsplash.com/photos/6mNKUrwMwFk/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjcxNTU1MTE5&force=true"
+    curl -L -o "$2/lucas-segers-6mNKUrwMwFk-unsplash.jpg" "https://unsplash.com/photos/6mNKUrwMwFk/download?ixid=MnwxMjA3fDB8MXxhbGx8fHx8fHx8fHwxNjcxNTU1MTE5&force=true"
 fi
