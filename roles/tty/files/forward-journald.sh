2d933896331bdc0d0db853b3872bb0eb27b76b67
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2022-12-20 15:26:14 +0100

    Newlines

--- roles/tty/files/forward-journald.sh
+++ roles/tty/files/forward-journald.sh
@@ -1,6 +1,6 @@
 #! /bin/bash
 
 mkdir -p /etc/systemd/journald.conf.d/
-echo -e "[Journal]\nForwardToConsole=yesTTYPath=/dev/tty12MaxLevelConsole=info" > /etc/systemd/journald.conf.d/fw-tty12.conf
+echo -e "[Journal]\nForwardToConsole=yes\nTTYPath=/dev/tty12\nMaxLevelConsole=info\n" > /etc/systemd/journald.conf.d/fw-tty12.conf
 
 systemctl restart systemd-journald.service
