a0e468f585cbb92b507d8de32bf735bac5c25946
Steven de Jong 🍟 <sdejong@cloudsuite.com>
2024-03-04 16:24:22 +0100

    Wallpaper switcher

--- .config/systemd/user/scripts/wallpaper.sh
+++ .config/systemd/user/scripts/wallpaper.sh
@@ -26,15 +26,35 @@ function find_wallpapers {
 wallpapers_list=()
 find_wallpapers "$WALLPAPERS_DIR"
 
-if [[ ${#wallpapers_list[@]} -gt 0 ]]; then
-    RANDOM_INDEX=$((RANDOM % ${#wallpapers_list[@]}))
-    RANDOM_WALLPAPER="${wallpapers_list[RANDOM_INDEX]}"
-    echo "Selected wallpaper: $RANDOM_WALLPAPER" >> "$LOG_FILE"
-    feh --bg-fill "$RANDOM_WALLPAPER"
+if [[ $# -gt 0 && ($1 == "-s" || $1 == "--select") ]]; then
+    if [[ ${#wallpapers_list[@]} -gt 0 ]]; then
+        echo "Select a wallpaper:"
+        for (( i=0; i<${#wallpapers_list[@]}; i++ )); do
+            echo "$((i+1)). ${wallpapers_list[i]}"
+        done
+        read -p "Enter the number of the wallpaper: " choice
+        if [[ $choice -ge 1 && $choice -le ${#wallpapers_list[@]} ]]; then
+            SELECTED_WALLPAPER="${wallpapers_list[choice-1]}"
+            echo "Selected wallpaper: $SELECTED_WALLPAPER" >> "$LOG_FILE"
+            feh --bg-fill "$SELECTED_WALLPAPER"
+            notify-send -i monitor 'Wallpaper' "changed to $SELECTED_WALLPAPER"
+        else
+            echo "Invalid choice." >> "$LOG_FILE"
+        fi
+    else
+        echo "No wallpapers found in the specified folder." >> "$LOG_FILE"
+    fi
 else
-    echo "No wallpapers found in the specified folder." >> "$LOG_FILE"
+    if [[ ${#wallpapers_list[@]} -gt 0 ]]; then
+        RANDOM_INDEX=$((RANDOM % ${#wallpapers_list[@]}))
+        RANDOM_WALLPAPER="${wallpapers_list[RANDOM_INDEX]}"
+        echo "Selected wallpaper: $RANDOM_WALLPAPER" >> "$LOG_FILE"
+        feh --bg-fill "$RANDOM_WALLPAPER"
+        notify-send -i monitor 'Wallpaper' "changed to $RANDOM_WALLPAPER"
+    else
+        echo "No wallpapers found in the specified folder." >> "$LOG_FILE"
+    fi
 fi
 
 echo "=====================" >> "$LOG_FILE"
 
-notify-send -i monitor 'Wallpaper' 'changed'
