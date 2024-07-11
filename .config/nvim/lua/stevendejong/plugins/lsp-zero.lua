a24a1ce4a167d0ff1b30df4895229e1f073ecbcf
Steven de jong 🍟 <sdejong@cloudsuite.com>
2024-07-11 18:14:41 +0200

    update

--- .config/nvim/lua/stevendejong/plugins/lsp-zero.lua
+++ .config/nvim/lua/stevendejong/plugins/lsp-zero.lua
@@ -8,6 +8,7 @@ return {
     {"neovim/nvim-lspconfig"},
     {"williamboman/mason.nvim"},
     {"williamboman/mason-lspconfig.nvim"},
+    {"davidosomething/format-ts-errors.nvim"},
 
     -- Autocompletion
     {"hrsh7th/nvim-cmp"},
@@ -88,6 +89,46 @@ return {
         ["<C-y>"] = cmp.mapping.confirm({ select = true }),
         ["<C-Space>"] = cmp.mapping.complete(),
       }),
+    })
+
+    local lspconfig = require("lspconfig")
+    lspconfig.tsserver.setup({
+      handlers = {
+        ["textDocument/publishDiagnostics"] = function(
+          _,
+          result,
+          ctx,
+          config
+        )
+          if result.diagnostics == nil then
+            return
+          end
+
+          -- ignore some tsserver diagnostics
+          local idx = 1
+          while idx <= #result.diagnostics do
+            local entry = result.diagnostics[idx]
+
+            local formatter = require('format-ts-errors')[entry.code]
+            entry.message = formatter and formatter(entry.message) or entry.message
+
+            -- codes: https://github.com/microsoft/TypeScript/blob/main/src/compiler/diagnosticMessages.json
+            if entry.code == 80001 then
+              -- { message = "File is a CommonJS module; it may be converted to an ES module.", }
+              table.remove(result.diagnostics, idx)
+            else
+              idx = idx + 1
+            end
+          end
+
+          vim.lsp.diagnostic.on_publish_diagnostics(
+            _,
+            result,
+            ctx,
+            config
+          )
+        end,
+      },
     })
   end,
 }
